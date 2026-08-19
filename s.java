0fdf1328ca853df2a58c0fddec457a10e0780304
Deekshith <226568086+notgamermx@users.noreply.github.com>
2026-08-20 01:39:38 +0530

    Create s.java

---
+++ s.java
@@ -0,0 +1,7 @@
+class HelloWorld {       
+}
+// import statements (optional)
+// class declaration
+public static void main(String[] args) {   // main method - entry point
+System.out.println("Hello, World!");   // statements
+}
