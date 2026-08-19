0bab86e4462a5584696fa9c0a2e37f7a1ffd40b7
Deekshith <226568086+notgamermx@users.noreply.github.com>
2026-08-20 01:40:36 +0530

    Delete s.java

--- s.java
+++
@@ -1,7 +0,0 @@
-class HelloWorld {       
-}
-// import statements (optional)
-// class declaration
-public static void main(String[] args) {   // main method - entry point
-System.out.println("Hello, World!");   // statements
-}
